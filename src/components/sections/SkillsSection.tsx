f1ba75c16eb18964ea2f7a16876ecc1de6f2c15b
Vinayak Deshmuk <vinayakdeshmuk191120033@gmail.com>
2025-08-15 08:49:15 +0000

    "use client" import React, { useEffect, useState, memo } from 'react';

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { OrbitingSkills } from '../OrbitingSkills';
+import OrbitingSkills from '../OrbitingSkills';
 
 export function SkillsSection() {
     return (
